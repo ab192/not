d89c47dcf2e9e21970950a776e3f6727e7b4e7c1
ab192 <77217911+ab192@users.noreply.github.com>
2021-01-09 22:11:18 -0600

    Create WurstplusUtil.java

---
+++ src/main/java/me/travis/wurstplus/wurstplustwo/util/WurstplusUtil.java
@@ -0,0 +1,8 @@
+package me.travis.wurstplus.wurstplustwo.util;
+
+import net.minecraft.client.Minecraft;
+
+public class WurstplusUtil {
+
+    public static final Minecraft mc = Minecraft.getMinecraft();
+}
