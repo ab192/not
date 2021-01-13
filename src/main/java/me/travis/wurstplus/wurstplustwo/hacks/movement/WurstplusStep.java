02c62ca7e4c890a594784ae0b8cca36878646f02
ab192 <77217911+ab192@users.noreply.github.com>
2021-01-12 22:30:21 -0600

    Update WurstplusStep.java

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/movement/WurstplusStep.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/movement/WurstplusStep.java
@@ -7,94 +7,78 @@ import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 
 public class WurstplusStep extends WurstplusHack {
-    
+
     public WurstplusStep() {
         super(WurstplusCategory.WURSTPLUS_MOVEMENT);
 
-		this.name        = "Step";
-		this.tag         = "Step";
-		this.description = "Move up / down block big";
+        this.name = "Step";
+        this.tag = "Step";
+        this.description = "Move up / down block big";
     }
 
     WurstplusSetting mode = create("Mode", "StepMode", "Normal", combobox("Normal", "Vanilla"));
 
-		
+
     @Override
     public void disable() {
-    if (mc.player == null) return;
-	    mc.player.stepHeight = 0.6f;
+        if ((mc.player == null) || (mc.world == null)) return;
+        mc.player.stepHeight = 0.6F;
     }
-	
+
+
     @Override
     public void update() {
-             if (mc.player == null) return;           
-
-        if (!mc.player.collidedHorizontally && mode.in("Normal")) return;
-        if (!mc.player.onGround || mc.player.isOnLadder() || mc.player.isInWater() || mc.player.isInLava() || mc.player.movementInput.jump || mc.player.noClip) return;
-        if (mc.player.moveForward == 0 && mc.player.moveStrafing == 0) return;
-
-        final double n = get_n_normal();
-
+        if ((mc.player == null) || (mc.world == null)) return;
         if (mode.in("Normal")) {
-
-            if (n < 0 || n > 2) return;
-
-            if (n == 2.0) {
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.42, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.78, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.63, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.51, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.9, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.21, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.45, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.43, mc.player.posZ, mc.player.onGround));
-                mc.player.setPosition(mc.player.posX, mc.player.posY + 2.0, mc.player.posZ);
+            if ((mc.player.moveForward != 0) && (mc.player.moveStrafing != 0) && mc.player.collidedHorizontally) {
+                if (mc.player.onGround && !mc.player.isOnLadder() && !mc.player.isInWater() && !mc.player.isInLava() && !mc.player.movementInput.jump && !mc.player.noClip) {
+                    double n = getNormal();
+                    if ((n < 0.0D) || (n > 2.0D)) {
+                        return;
+                    }
+                    if (n == 2.0D) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.42, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.78, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.63, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.51, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.9, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.21, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.45, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.43, mc.player.posZ, mc.player.onGround));
+                        mc.player.setPosition(mc.player.posX, mc.player.posY + 2.0, mc.player.posZ);
+                    } else if (n == 1.5D) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805212, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.00133597911214, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.16610926093821, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.24918707874468, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.1707870772188, mc.player.posZ, mc.player.onGround));
+                        mc.player.setPosition(mc.player.posX, mc.player.posY + 1.0, mc.player.posZ);
+                    } else if (n == 1.0D) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805212, mc.player.posZ, mc.player.onGround));
+                        mc.player.setPosition(mc.player.posX, mc.player.posY + 1.0, mc.player.posZ);
+                    }
+                }
             }
-            if (n == 1.5) {
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805212, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.00133597911214, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.16610926093821, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.24918707874468, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.1707870772188, mc.player.posZ, mc.player.onGround));
-                mc.player.setPosition(mc.player.posX, mc.player.posY + 1.0, mc.player.posZ);
-            }
-            if (n == 1.0) {
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698, mc.player.posZ, mc.player.onGround));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805212, mc.player.posZ, mc.player.onGround));
-                mc.player.setPosition(mc.player.posX, mc.player.posY + 1.0, mc.player.posZ);
-            }
-
+        } else if (mode.in("Vanilla")) {
+            mc.player.stepHeight = 2.0F;
         }
-
-    }
-		    if (mode.in("Vanilla)) {
-		    mc.player.stepHeight = 2.0f
-		    }
-		}
-
-    public double get_n_normal() {
-
-        mc.player.stepHeight = 0.5f;
-
-        double max_y = -1;
-
-        final AxisAlignedBB grow = mc.player.getEntityBoundingBox().offset(0, 0.05, 0).grow(0.05);
-
-        if (!mc.world.getCollisionBoxes(mc.player, grow.offset(0, 2, 0)).isEmpty()) return 100;
-
-        for (final AxisAlignedBB aabb : mc.world.getCollisionBoxes(mc.player, grow)) {
-
-            if (aabb.maxY > max_y) {
-                max_y = aabb.maxY;
-            }
-
-        }
-
-        return max_y - mc.player.posY;
-
     }
 
-    
 
-}
+    public double getNormal() {
+        mc.player.stepHeight = 0.6F;
+        AxisAlignedBB grow = mc.player.getEntityBoundingBox().offset(0, 0.05, 0).grow(0.05);
+
+        if (!mc.world.getCollisionBoxes(mc.player, grow.offset(0, 2, 0)).isEmpty()) {
+            return 100;
+        }
+        double maxY = -1;
+        for (AxisAlignedBB aabb : mc.world.getCollisionBoxes(mc.player, grow)) {
+            if (aabb.maxY > maxY) {
+                maxY = aabb.maxY;
+            }
+        }
+        return maxY - mc.player.posY;
+    }
