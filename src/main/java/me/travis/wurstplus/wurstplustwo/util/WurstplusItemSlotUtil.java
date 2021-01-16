faaf82b45d6868dfbd3918d0d231d125f8b0ab4c
ab192 <77217911+ab192@users.noreply.github.com>
2021-01-15 19:15:18 -0600

    Create WurstplusItemSlotUtil.java

---
+++ src/main/java/me/travis/wurstplus/wurstplustwo/util/WurstplusItemSlotUtil.java
@@ -0,0 +1,16 @@
+package me.travis.wurstplus.wurstplustwo.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
+public class WurstplusItemSlotUtil {
+
+ private int get_item_slot(Item input) {
+        if (input == mc.player.getHeldItemOffhand().getItem()) return -1;
+        for(int i = 36; i >= 0; i--) {
+            final Item item = mc.player.inventory.getStackInSlot(i).getItem();
+            }
+    }
+    
